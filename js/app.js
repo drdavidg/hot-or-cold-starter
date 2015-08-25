a95a0f49e9fbcd01b57ab2fd785f91b1584fdf10
David Gonen <david@curiousminds.com>
2015-08-25 12:39:59 +0300

    clicking the guess button makes the form submit, which is refreshing the page and clearing out the message i'm displaying about how close the guess is...

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
 
 $(document).ready(function(){
 
+	var x = secretNumber(); //set global variable of the secretNumber number to be guessed
+	console.log(x);
+
+
+	$('nav').on('click', 'ul > li > a.new', newGame );
+	// $('form').on('click', '#guessButton', checkGuess );
+	$('form').submit(checkGuess);
+
 
 	function newGame() {
+		x = secretNumber(); // set the global variable to a new secret number
+		console.log(x);
+	}
 
+	function checkGuess() {
+
+
+		var guess = $('#userGuess').val();
+		if (guess == x) {
+			$('h2#feedback').text("BINGO!!!");
+		}
+		else if (guess > x) {
+			$('h2#feedback').text("Too High");
+		}
+		else if (guess < x) {
+			$('h2#feedback').text("Too Low");
+		}
+		console.log(guess);
 
 	}
 
-
-	function generateNumber() {
-
-
+	function secretNumber() {
+		return Math.floor(Math.random()*100);
 	}
-	
 
 
 	/*--- Display information modal box ---*/
